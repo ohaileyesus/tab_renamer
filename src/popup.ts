51dc9fc7ae5a0cce8b1ce6bbcab0171ab121a382
Omega Haileyesus <ohaileyesus@twitter.com>
2022-09-30 16:39:39 -0400

    input tab name works. all thats left is refresh

--- src/popup.ts
+++ src/popup.ts
@@ -1,10 +1,29 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById('changeColor')
+const saveButton = document.getElementById("saveButton");
+const tabNameInput = document.getElementById("tabName");
 
-console.log(chrome.tabs)
-console.log(chrome.tabGroups)
-console.log(changeColor)
+async function getCurrentTab() {
+  let [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 
-// chrome.storage.sync.get('color', ({ color }: { color: any }) => {
-//   changeColor.style.backgroundColor = color
-// })
+  return tab;
+}
+
+saveButton?.addEventListener("click", async () => {
+  if (!tabNameInput) return;
+
+  const tabName = (tabNameInput as HTMLInputElement).value;
+  if (!tabName) return;
+
+  const tabId = (await getCurrentTab())?.id;
+  if (!tabId) return;
+
+  chrome.scripting.executeScript({
+    args: [tabName],
+    target: { tabId: tabId },
+    func: (tabName) => {
+      document.title = tabName;
+    },
+  });
+});
