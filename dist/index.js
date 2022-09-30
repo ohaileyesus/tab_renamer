c776a09b026c58bf0f028eb7df3d53b49356439a
Matt Granmoe <mgranmoe@twitter.com>
2022-09-30 11:59:35 -0500

    Basic hello world extension working with TS

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(name) {
-    return `Hello ${name}`;
-}
-exports.default = default_1;
---
+++ src/popup.ts
@@ -0,0 +1,10 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById('changeColor')
+
+console.log(chrome.tabs)
+console.log(chrome.tabGroups)
+console.log(changeColor)
+
+// chrome.storage.sync.get('color', ({ color }: { color: any }) => {
+//   changeColor.style.backgroundColor = color
+// })
